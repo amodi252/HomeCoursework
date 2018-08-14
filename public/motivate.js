9a763d512d381d5f573a3b63934a01eb323ec712
Ayush Modi <amodi252@gmail.com>
2018-08-15 00:02:34 +0100

    Get images from 3 sources and display masonry div. TODO: Make masonry beaut. Reload images, get API/new sources? Make board max-height and width. Transitions

--- public/motivate.js
+++ public/motivate.js
@@ -1,34 +1,54 @@
 var imgs = [];
 
 init();
-
+var exam = [];
 function init () {
     var boardInfo = ['motivateamazebegreat/inspirational-quotes',
                      'joabauer/inspiring-quotes',
                      'sayingimages/motivational-quotes'
     ];
-    boardInfo.forEach(function(source) {
-         getImages(source);
+
+    boardInfo.forEach((source, index) => {
+        getImages(source, () => {
+            if (index === boardInfo.length - 1) {
+                getRandom();
+            }
+        });
     });
-    console.log(imgs);
+}
+$(window).on('load', function (){
+    // $('.grid').masonry({
+    //   // options
+    //   itemSelector: '.grid-item',
+    //   percentPosition: true,
+    //   columnWidth: 1
+    // });
+});
+function getImages (source, callback) {
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        url: "https://api.pinterest.com/v3/pidgets/boards/" + source + "/pins/",
+        success: function(inp) {
+            for (var i = 0; i < 50; i++) {
+                var a = inp.data.pins[i].images["237x"].url;
+                a = a.replace('/237x/','/564x/');
+                imgs.push([a, inp.data.pins[i].description]);
+            }
+            callback();
+        }
+    });
 }
 
-$('.grid').masonry({
-  // options
-  itemSelector: '.grid-item',
-  //columnWidth: 160;
-});
-function getImages (source) {
-    $.ajax({
-     type: "GET",
-     dataType: "jsonp",
-     url: "https://api.pinterest.com/v3/pidgets/boards/" + source +"/pins/",
-     success: function(inp) {
-       for (var i = 0; i < 50; i++) {
-         var a = inp.data.pins[i].images["237x"].url;
-         a = a.replace('/237x/','/564x/');
-         imgs.push([a, inp.data.pins[i].description]);
-        }
-     }
-    });
+function getRandom () {
+    for (var i = 0; i < 10; i++) {
+        var rand = Math.floor(Math.random() * imgs.length);
+
+        $('.grid').append(
+            `<div class="grid-item">
+                <img src="${imgs[rand][0]}" alt="${imgs[rand][1]}">
+            </div>`
+        )
+        imgs.splice(rand,1);
+    }
 }
