a747f516606d3cc11456340c553ab5f0e6868c4f
Ayush Modi <amodi252@gmail.com>
2018-08-17 01:27:49 +0100

    Beautify masonry, understand promises. TODO: Make board max-height, add transitions, add a quote in remaining space from good quote maker

--- public/motivate.js
+++ public/motivate.js
@@ -2,41 +2,51 @@ var imgs = [];
 
 init();
 var exam = [];
+var allItems;
 function init () {
     var boardInfo = ['motivateamazebegreat/inspirational-quotes',
                      'joabauer/inspiring-quotes',
                      'sayingimages/motivational-quotes'
     ];
 
-    boardInfo.forEach((source, index) => {
-        getImages(source, () => {
-            if (index === boardInfo.length - 1) {
-                getRandom();
-            }
+    var promises = [];
+    boardInfo.forEach(source => {
+        promises.push(getImages(source));
+    });
+    Promise.all(promises).then(function() {
+        return getRandom();
+    }, function(err){
+        console.log("huh " + err);
+    }).then(function() {
+        $(window).on("load", function () {
+            allItems = $(".item");
+            $.each(allItems, function(i,item){
+
+                console.log($(item));
+                resizeInstance($(item));
+            });
         });
+        // allItems.forEach(function(item) {
+        //   imagesLoaded(item, resizeInstance);
+        // });
     });
 }
-$(window).on('load', function (){
-    // $('.grid').masonry({
-    //   // options
-    //   itemSelector: '.grid-item',
-    //   percentPosition: true,
-    //   columnWidth: 1
-    // });
-});
+
 function getImages (source, callback) {
-    $.ajax({
-        type: "GET",
-        dataType: "jsonp",
-        url: "https://api.pinterest.com/v3/pidgets/boards/" + source + "/pins/",
-        success: function(inp) {
-            for (var i = 0; i < 50; i++) {
-                var a = inp.data.pins[i].images["237x"].url;
-                a = a.replace('/237x/','/564x/');
-                imgs.push([a, inp.data.pins[i].description]);
+    return new Promise(function(res,rej) {
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            url: "https://api.pinterest.com/v3/pidgets/boards/" + source + "/pins/",
+            success: function(inp) {
+                for (var i = 0; i < inp.data.pins.length; i++) {
+                    var a = inp.data.pins[i].images["237x"].url;
+                    a = a.replace('/237x/','/564x/');
+                    imgs.push([a, inp.data.pins[i].description]);
+                    res(true);
+                }
             }
-            callback();
-        }
+        });
     });
 }
 
@@ -45,10 +55,42 @@ function getRandom () {
         var rand = Math.floor(Math.random() * imgs.length);
 
         $('.grid').append(
-            `<div class="grid-item">
-                <img src="${imgs[rand][0]}" alt="${imgs[rand][1]}">
+            `<div class="item photo">
+                <div class="content">
+                    <img class="photothumb" src="${imgs[rand][0]}" alt="${imgs[rand][1]}">
+                </div>
             </div>`
         )
         imgs.splice(rand,1);
     }
+}
+
+window.onload = resizeAllGridItems();
+$(window).on("resize", resizeAllGridItems);
+
+
+function resizeInstance(instance){
+	item = instance.first();
+    resizeGridItem(item);
+}
+
+function resizeAllGridItems(){
+    // allItems = $(".item");
+    // allItems.forEach(function(item) {
+    //   imagesLoaded(item, resizeInstance);
+    // });
+
+    allItems = $(".item");
+    $.each(allItems, function(i,item){
+        console.log(instance);
+        resizeInstance($(item));
+    });
+}
+
+function resizeGridItem(item){
+  $grid = $(".grid").first();
+  rowHeight = parseInt($grid.css('grid-auto-rows'));
+  rowGap = parseInt($grid.css('grid-row-gap'));
+  rowSpan = Math.ceil((item.children('.content').get(0).getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.css('gridRowEnd', 'span '+rowSpan);
 }
